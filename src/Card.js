9d9843b6421f595224e239b8525e2eff9e63512f
zachalbert <zachalbert@gmail.com>
2020-12-29 19:53:50 -0800

    Tweakin

--- src/Card.js
+++ src/Card.js
@@ -7,7 +7,7 @@ function Card({
     <div className={`theme ${theme}`}>
       <div className={`py-10 p-6 background--${cardVariant}`}>
         <div className={`card card--${cardVariant}`}>
-          <h1 className="mb-2">{title}</h1>
+          <h1 className={`mb-2 text-gray-500 heading--${cardVariant}`}>{title}</h1>
           <p>
             Lorem ipsum dolor sit amet.
           </p>
